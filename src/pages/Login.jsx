976ece77d479ee88cce1ebd505a561a506edb525
MaximoFC <maximofcallejas@gmail.com>
2025-06-16 17:12:42 -0300

    tailwindcss fixed and login in development

---
+++ src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { loginEmployee } from "../services/authService";
+
+function Login() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await loginEmployee(name, password);
+            localStorage.setItem('token', res.token);
+            console.log('Successful login', res.employee);
+            //redirect
+            alert("Iniciaste sesión")
+        } catch (error) {
+            setError(error.message || 'Login error');
+        }
+    };
+
+    return (
+        <div className="p-8">
+            <form 
+                onSubmit={handleSubmit}
+                className="flex flex-col justify-center gap-4 border-1 border-black rounded-xl p-6"
+            >
+                <h2 className="">Login</h2>
+                <input 
+                    type="text" 
+                    placeholder="Nombre"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                    className="border-1 border-black rounded-xl p-2"
+                />
+                <input 
+                    type="password" 
+                    placeholder="Contraseña"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    className="border-1 border-black rounded-xl p-2"
+                />
+                <button 
+                    type="submit"
+                    className="border-1 border-red-500 p-2 rounded-xl cursor-pointer hover:bg-red-500 hover:text-white"
+                >
+                    Ingresar
+                </button>
+            </form>
+            {error && <p>{error}</p>}
+        </div>
+    )
+}
+
+export default Login;
