851e42caf23642bf5d57a5e77239cf11f8364848
MaximoFC <maximofcallejas@gmail.com>
2025-06-24 13:25:30 -0300

    ui improvements

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,20 @@
 import { Link, useLocation } from "react-router-dom";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { GoPeople, GoTools } from "react-icons/go";
-import { GrDocumentText } from "react-icons/gr";
 import { BsBox2 } from "react-icons/bs";
 import { MdOutlineAttachMoney } from "react-icons/md";
+import { TfiClipboard } from "react-icons/tfi";
 
 const Sidebar = () => {
     const { pathname } = useLocation();
 
     const linkClasses = (path) =>
-        `flex gap-4 items-center px-2 py-2 rounded-md hover:bg-gray-200 transition ${
-            pathname === path ? "bg-gray-200 font-semibold" : ""
+        `flex gap-4 items-center px-2 py-2 rounded-md border-1 border-white hover:border-gray-300 transition ${
+            pathname === path ? "bg-red-500 text-white font-semibold" : ""
         }`;
 
     return (
-        <div className="flex flex-col h-dvh p-4 gap-4 border-r-1 border-gray-300">
+        <div className="flex flex-col h-dvh p-4 gap-2 border-r-1 border-gray-300">
             <Link to="/dashboard" className={linkClasses("/dashboard")}>
                 <LuLayoutDashboard className="w-6 h-6" /> Dashboard
             </Link>
@@ -25,10 +25,10 @@ const Sidebar = () => {
                 <GoTools className="w-6 h-6" /> Trabajos
             </Link>
             <Link to="/presupuestos" className={linkClasses("/presupuestos")}>
-                <GrDocumentText className="w-6 h-6" /> Presupuestos
+                <TfiClipboard className="w-5 h-5" /> Presupuestos
             </Link>
             <Link to="/repuestos" className={linkClasses("/repuestos")}>
-                <BsBox2 className="w-6 h-6" /> Inventario
+                <BsBox2 className="w-5 h-5" /> Inventario
             </Link>
             <Link to="/caja" className={linkClasses("/caja")}>
                 <MdOutlineAttachMoney className="w-6 h-6" /> Caja
