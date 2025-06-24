64150efdba276e1e494cf2b07b94a9318da9ff79
MaximoFC <maximofcallejas@gmail.com>
2025-06-23 23:20:54 -0300

    navbar placeholder dynamic

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -7,9 +7,14 @@ export const useSearch = () => useContext(SearchContext);
 export const SearchProvider = ({ children }) => {
     const [searchTerm, setSearchTerm] = useState('');
     const [onSearch, setOnSearch] = useState(null);
+    const [searchPlaceHolder, setSearchPlaceHolder] = useState('Buscar cliente, trabajo o repuesto');
 
     return (
-        <SearchContext.Provider value={{ searchTerm, setSearchTerm, onSearch, setOnSearch }}>
+        <SearchContext.Provider value={{
+            searchTerm, setSearchTerm,
+            onSearch, setOnSearch,
+            searchPlaceHolder, setSearchPlaceHolder
+            }}>
             {children}
         </SearchContext.Provider>
     );
